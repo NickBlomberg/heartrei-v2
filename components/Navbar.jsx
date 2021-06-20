82c4745e18d1376f81b3e5f92c9cf06fb7d8bd6f
Nick Blomberg <nick.blomberg@gmail.com>
2021-06-20 08:50:48 +0100

    Add navigation links to Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,25 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { Flex, Heading } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Stack,
+  Heading,
+  Link as ChakraLink,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const handleToggle = () => (isOpen ? onClose() : onOpen())
+
   return (
     <Flex
       as="nav"
       align="center"
+      justify="space-between"
+      wrap="wrap"
       padding={5}
       bg="gray.100"
       color="gray.700"
@@ -28,6 +41,31 @@ export default function Navbar() {
           </Flex>
         </a>
       </Link>
+
+      <Box display={{ base: 'block', md: 'none' }} onClick={handleToggle}>
+        <HamburgerIcon />
+      </Box>
+
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        display={{ base: isOpen ? 'block' : 'none', md: 'flex' }}
+        width={{ base: 'full', md: 'auto' }}
+        spacing={{ base: 10, md: 9 }}
+        mt={{ base: 8, md: 0 }}
+        mr={{ base: 0, md: 5 }}
+      >
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/therapies">Therapies</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+      </Stack>
     </Flex>
   )
 }
+
+const NavLink = ({ children, to }) => (
+  <Link href={to} passHref>
+    <ChakraLink display="block">{children}</ChakraLink>
+  </Link>
+)
