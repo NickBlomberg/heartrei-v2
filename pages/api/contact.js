636decfbb7d44663c9d9f205c21f596ef87e2c58
Nick Blomberg <nick.blomberg@gmail.com>
2021-06-23 10:45:16 +0100

    Enable messages to be sent via SendGrid

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,19 @@
+import sgMail from '@sendgrid/mail'
+
 export default async (req, res) => {
-  res.status(200).json({ message: 'Contact form sent successfully' })
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'heartrei.therapies@gmail.com',
+    from: 'heartrei.therapies@gmail.com',
+    subject: 'New contact form submission',
+    name: 'HeartRei Therapies',
+    text: JSON.stringify(req.body),
+  }
+
+  try {
+    await sgMail.send(msg)
+    res.status(200).json({ message: 'Contact form sent successfully' })
+  } catch (error) {
+    res.status(500).json({ error: 'Server error when sending contact email' })
+  }
 }
