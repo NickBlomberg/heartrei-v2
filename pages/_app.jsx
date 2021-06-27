62f24ca83f2259757be05281eba2d026b280a42a
Nick Blomberg <nick.blomberg@gmail.com>
2021-06-27 18:00:27 +0100

    Add analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import '@fontsource/courgette/400.css'
@@ -9,6 +10,13 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Script
+        data-host="https://microanalytics.io"
+        data-dnt="false"
+        src="https://microanalytics.io/js/script.js"
+        id="ZwSg9rf6GA"
+        strategy="lazyOnload"
+      />
     </ChakraProvider>
   )
 }
