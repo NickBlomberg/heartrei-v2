c808c61908628ffb3da7b4983b24dd9d8ac74a45
Nick Blomberg <nick.blomberg@gmail.com>
2021-06-27 12:58:35 +0100

    Add back button to blog posts

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,5 +1,6 @@
-import { Box, Container, Flex, Heading, Text } from '@chakra-ui/react'
-import { CalendarIcon } from '@chakra-ui/icons'
+import { useRouter } from 'next/router'
+import { Box, Container, Flex, Heading, Text, Button } from '@chakra-ui/react'
+import { CalendarIcon, ArrowBackIcon } from '@chakra-ui/icons'
 import Layout from '@/components/Layout'
 import ContentfulBlogPost from '@/contentful/BlogPost'
 
@@ -11,6 +12,7 @@ export default function Post({ post }) {
           {post.title}
         </Heading>
         <Box px={{ base: 3, md: 16 }}>
+          <BackButton mb={8} />
           <Flex direction="row" align="center" mb={2}>
             <CalendarIcon color="gray.400" mr={2} />
             <Text>
@@ -23,9 +25,23 @@ export default function Post({ post }) {
           </Flex>
 
           <Text fontSize="xl">{post.content}</Text>
+          <BackButton mt={8} />
         </Box>
       </Container>
     </Layout>
+  )
+}
+
+const BackButton = ({ ...rest }) => {
+  const router = useRouter()
+  return (
+    <Button
+      leftIcon={<ArrowBackIcon />}
+      onClick={() => router.back()}
+      {...rest}
+    >
+      Back
+    </Button>
   )
 }
 
