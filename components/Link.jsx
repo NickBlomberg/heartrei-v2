c106559a74801759ed8df3224d02b4f9d5f50591
Nick Blomberg <nick.blomberg@gmail.com>
2021-06-27 13:09:28 +0100

    Provide default href to Link component

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,7 +1,7 @@
 import { Link as ChakraLink } from '@chakra-ui/react'
 import NextLink from 'next/link'
 
-export default function Link({ children, href, ...rest }) {
+export default function Link({ children, href = '/', ...rest }) {
   return (
     <NextLink href={href} passHref>
       <ChakraLink {...rest}>{children}</ChakraLink>
